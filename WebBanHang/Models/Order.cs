92912f108836cc668b404024bd6841977b50754a
npvuong2004 <npvuong2004@gmail.com>
2024-06-18 11:18:26 +0700

    df

---
+++ WebBanHang/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanHang.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string KhachHangName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Total { get; set; }
+        public string State { get; set; }
+
+    }
+}
